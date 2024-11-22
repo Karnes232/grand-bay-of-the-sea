a0074f050e39212e047531dc3db11386ee965316
James Karnes <karnes.james@gmail.com>
2024-11-22 09:32:34 -0400

    testing for netlify forms

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Add this line
+  // output: 'export', // Add this line
   trailingSlash: true,
     reactStrictMode: true,
     images: {
