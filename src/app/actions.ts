ec008ce8b529f87d1336e70dd1bea71660fcb4b4
James Karnes <karnes.james@gmail.com>
2024-11-22 14:08:02 -0400

    testing form with netlify

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -34,7 +34,7 @@ export async function submitForm(formData) {
 
   //   })
   try {
-    const response = await fetch('/', {
+    const response = await fetch('/__forms.html', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({
