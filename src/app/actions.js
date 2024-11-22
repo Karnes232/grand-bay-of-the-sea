dfc0bba52d62b9d6ee29a95096db90bfa761cc29
James Karnes <karnes.james@gmail.com>
2024-11-22 13:42:44 -0400

    testing form with netlify

---
+++ src/app/actions.js
@@ -0,0 +1,38 @@
+"use server"
+
+export async function submitForm(formData) {
+    console.log(formData)
+  const name = formData.get("name")
+  const email = formData.get("email")
+  const hotel = formData.get("hotel")
+  const message = formData.get("message")
+  const form = document.getElementById("contact")
+  const newFormData = new FormData(form);
+  const formDataObj = {};
+  newFormData.forEach((value, key) => (formDataObj[key] = value));
+//   const encode = (data: any) => {
+//     return Object.keys(data)
+//       .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+//       .join("&")
+//   }
+
+  try {
+    const response = await fetch("/__forms.html", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(newFormData).toString(),
+    //   body: encode({
+    //     "form-name": "contact",
+    //     name,
+    //     email,
+    //     hotel,
+    //     message,
+    //   }),
+    })
+
+    return { success: response.ok }
+  } catch (error) {
+    console.error("Form submission error:", error)
+    return { success: false }
+  }
+}
