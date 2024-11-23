56ded7605d4857d17c283b1079c0e14eb8a9c166
James Karnes <karnes.james@gmail.com>
2024-11-23 09:24:20 -0400

    created thank you page

--- src/app/thankyou/page.tsx
+++ src/app/thankyou/page.tsx
@@ -1,5 +1,7 @@
+import SwiperCarousel from "@/components/BackgroundCarouselComponents/SwiperCarousel"
 import ContactForm from "@/components/ContactForm/ContactForm"
 import HeroComponent from "@/components/HeroComponent/HeroComponent"
+import RichText from "@/components/RichTextComponents/RichText"
 import { searchEntries } from "@/lib/contentful"
 import { Metadata, ResolvingMetadata } from "next"
 
@@ -15,7 +17,7 @@ export async function generateMetadata(
     description: String(seoSearchResults.items[0].fields.description),
     keywords: seoSearchResults.items[0].fields.keywords as string[],
     openGraph: {
-      url: "https://www.grandbay-puntacana.com/contact",
+      url: "https://www.grandbay-puntacana.com/thankyou",
       type: "website",
       title: String(seoSearchResults.items[0].fields.title),
       description: String(seoSearchResults.items[0].fields.description),
@@ -48,7 +50,7 @@ export async function generateMetadata(
       ],
     },
     alternates: {
-      canonical: "https://www.grandbay-puntacana.com/contact/",
+      canonical: "https://www.grandbay-puntacana.com/thankyou/",
     },
   }
 }
@@ -61,13 +63,42 @@ export default async function Home(props: any) {
     },
     ["fields.heroImage"],
   )
+  const email = await searchEntries(
+    "layout",
+    {
+      "fields.companyName": "Grand Bay of the Sea",
+    },
+    ["fields.email"],
+  )
   return (
     <main>
       <HeroComponent
         heroImage={`https:${(searchResults.items[0] as any).fields.heroImage.fields.file.url}`}
       />
       <div className="mt-[50vh] md:mt-[40vh] lg:mt-[70vh]" />
-      <ContactForm />
+      <div className="flex flex-col items-center justify-center max-w-xs xl:max-w-sm mx-auto min-h-[40vh] xl:min-h-[50vh]">
+        <div className="mb-10">
+          <div className="flex flex-col justify-center items-center text-slate-600 ">
+            <div className="text-2xl xl:text-4xl font-serif text-center mt-6">
+              Thank you {props.searchParams.name}, our team will reach out to
+              you shortly!
+            </div>
+
+            <div className="text-center text-sm xl:text-base mt-2 xl:mt-6">
+              Please feel free to{" "}
+              <a
+                href={`mailto:${email.items[0].fields.email as string}`}
+                aria-label="Gmail"
+                rel="noreferrer"
+                className="underline"
+              >
+                contact us
+              </a>{" "}
+              with any questions or concerns.
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
