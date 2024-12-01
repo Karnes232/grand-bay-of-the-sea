7145ad0b288257b9331b07790a7ee068076e1860
James Karnes <karnes.james@gmail.com>
2024-12-01 15:46:07 -0400

    added sharp

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,12 @@
-import type { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from "next"
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/tui/*',
+      userAgent: "*",
+      allow: "/",
+      disallow: "/tui/*",
     },
-    sitemap: 'https://www.grandbay-puntacana.com/sitemap.xml',
+    sitemap: "https://www.grandbay-puntacana.com/sitemap.xml",
   }
 }
