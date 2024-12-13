b0413d8195abc8033eef7ed6e0a0af7fe64699ab
James Karnes <karnes.james@gmail.com>
2024-12-13 17:23:13 -0400

    testing a placeholder for next image

--- src/components/HeroComponent/HeroComponent.tsx
+++ src/components/HeroComponent/HeroComponent.tsx
@@ -2,11 +2,15 @@ import React from "react"
 import Image from "next/image"
 import { headers } from "next/headers"
 import { isMobile } from "@/utils/isMobile"
+import { getPlaiceholder } from "plaiceholder"
 const HeroComponent = async ({ heroImage }: { heroImage: string }) => {
   const headersList = await headers()
   const userAgent = headersList.get("user-agent")
   const mobileCheck = isMobile(userAgent)
-
+  const buffer = await fetch(heroImage).then(async res => {
+    return Buffer.from(await res.arrayBuffer())
+  })
+  const { base64 } = await getPlaiceholder(buffer)
   return (
     <div className="absolute top-0 w-full h-[55vh] lg:h-[80vh]">
       <div className="relative h-[55vh] lg:h-[80vh] [clip-path:polygon(0%_0%,100%_0%,100%_50vh,0%_100%)] lg:[clip-path:polygon(0%_0%,100%_0%,100%_75vh,0%_100%)]">
@@ -17,6 +21,8 @@ const HeroComponent = async ({ heroImage }: { heroImage: string }) => {
           className="object-cover object-[40%_50%] z-0"
           priority
           quality={mobileCheck ? 25 : 75}
+          placeholder="blur"
+          blurDataURL={base64}
         />
         <div className="absolute inset-0 z-10 bg-[linear-gradient(to_bottom,rgba(245,246,252,0.52),rgba(0,0,0,0.73))]" />
       </div>
