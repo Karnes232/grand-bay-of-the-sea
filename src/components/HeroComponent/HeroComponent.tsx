49509ea4c9e09b3a48567047e2eb40d6e691ced2
James Karnes <karnes.james@gmail.com>
2024-12-13 14:57:55 -0400

    changing image quality depending if mobile

--- src/components/HeroComponent/HeroComponent.tsx
+++ src/components/HeroComponent/HeroComponent.tsx
@@ -1,6 +1,11 @@
 import React from "react"
 import Image from "next/image"
+import { headers } from "next/headers"
+import { isMobile } from "@/utils/isMobile"
 const HeroComponent = ({ heroImage }: { heroImage: string }) => {
+  const userAgent = headers().get("user-agent") || ""
+  const mobileCheck = isMobile(userAgent)
+
   return (
     <div className="absolute top-0 w-full h-[55vh] lg:h-[80vh]">
       <div className="relative h-[55vh] lg:h-[80vh] [clip-path:polygon(0%_0%,100%_0%,100%_50vh,0%_100%)] lg:[clip-path:polygon(0%_0%,100%_0%,100%_75vh,0%_100%)]">
@@ -10,7 +15,7 @@ const HeroComponent = ({ heroImage }: { heroImage: string }) => {
           fill
           className="object-cover object-[40%_50%] z-0"
           priority
-          quality={75}
+          quality={mobileCheck ? 25 : 75}
         />
         <div className="absolute inset-0 z-10 bg-[linear-gradient(to_bottom,rgba(245,246,252,0.52),rgba(0,0,0,0.73))]" />
       </div>
