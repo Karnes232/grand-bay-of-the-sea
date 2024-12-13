6e0dd9275533736a1ffd985cdaf0aa1000855cbc
James Karnes <karnes.james@gmail.com>
2024-12-13 16:09:26 -0400

    changed params to promise on all pages

--- src/app/(root)/courses/[slug]/page.tsx
+++ src/app/(root)/courses/[slug]/page.tsx
@@ -65,7 +65,7 @@ export async function generateMetadata(
   }
 }
 
-export default async function Page({ params }: { params: { slug: string } }) {
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params
   const course = await searchEntries("course", {
     "fields.slug": slug,
--- src/app/(root)/trips/[slug]/page.tsx
+++ src/app/(root)/trips/[slug]/page.tsx
@@ -68,7 +68,7 @@ export async function generateMetadata(
   }
 }
 
-export default async function Page({ params }: { params: { slug: string } }) {
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params
   const tour = await searchEntries(
     "tours",
