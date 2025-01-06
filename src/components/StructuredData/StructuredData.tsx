c19c63007dadd8945dbcfa587e0bef48f63da290
James Karnes <karnes.james@gmail.com>
2025-01-06 19:10:32 -0400

    updated schema for landing page

--- src/components/StructuredData/StructuredData.tsx
+++ src/components/StructuredData/StructuredData.tsx
@@ -1,24 +1,25 @@
 export function generateStructuredData() {
   return {
     "@context": "https://schema.org",
-    "@type": "Organization",
+    "@type": "LocalBusiness",
     name: "Grand Bay of the Sea",
-    description:
-      "Join our Scuba Divers on the reefs around Punta Cana and the surrounding area. To learn about our Dive Company, visit our website now.",
+    image:
+      "https://images.ctfassets.net/iqfmqk4smewk/4CYN3rPs0ryO4Gi4JDPm8e/156feb808cdad566332c11071dac1e09/pexels-leonardo-lamas-7001709.webp?h=250",
+    "@id": "",
     url: "https://www.grandbay-puntacana.com/",
-    logo: "https://www.grandbay-puntacana.com/static/logo-smaller-d5cb1a00f0a9e9d2de1108977be2b0af.png",
-    contactPoint: {
-      "@type": "ContactPoint",
-      telephone: "+18297239338",
-      contactType: "customer service",
-      availableLanguage: ["en", "es", "fr"],
+    telephone: "+18297239338",
+    address: {
+      "@type": "PostalAddress",
+      streetAddress: "",
+      addressLocality: "Punta Cana",
+      postalCode: "",
+      addressCountry: "DO",
     },
     sameAs: [
       "https://www.facebook.com/grandbaydivecenter/",
-      "https://twitter.com/GrandBayOfTheS1",
       "https://www.instagram.com/grandbayoftheseard/",
+      "https://twitter.com/GrandBayOfTheS1",
       "https://www.youtube.com/channel/UCpYWOhIwbVVLGYOxL0UAtTw",
-      "https://grandbay-puntacana.com/",
     ],
   }
 }
