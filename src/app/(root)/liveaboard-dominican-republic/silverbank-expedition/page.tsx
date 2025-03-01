ff385ec8551deefdb717cb676774aeabedaf63ab
James Karnes <karnes.james@gmail.com>
2025-03-01 14:01:59 -0400

    added silverbank and watch watching

--- src/app/(root)/liveaboard-dominican-republic/silverbank-expedition/page.tsx
+++ src/app/(root)/liveaboard-dominican-republic/silverbank-expedition/page.tsx
@@ -1,20 +1,17 @@
 import SwiperCarousel from "@/components/BackgroundCarouselComponents/SwiperCarousel"
 import BackgroundVideo from "@/components/BackgroundVideoComponent/BackgroundVideo"
-import CourseOverview from "@/components/CourseComponents/CourseOverview"
 import RichText from "@/components/RichTextComponents/RichText"
-import TripOverview from "@/components/TourOverviews/TripOverview"
-import { getAllEntries, searchEntries } from "@/lib/contentful"
+import { searchEntries } from "@/lib/contentful"
 import { Metadata, ResolvingMetadata } from "next"
 
 export async function generateMetadata(
   { params }: { params: Promise<{ slug: string }> },
   parent: ResolvingMetadata,
 ): Promise<Metadata> {
-  const { slug } = await params
   const seoSearchResults = await searchEntries(
     "tours",
     {
-      "fields.slug": slug,
+      "fields.slug": "silverbank-expedition",
     },
     [
       "fields.seoTitle",
@@ -22,15 +19,15 @@ export async function generateMetadata(
       "fields.seoKeywords",
       "fields.seoImage",
     ],
-    ["fishing-punta-cana", "shark-dive-punta-cana"],
+    ["fishing-punta-cana"],
   )
 
   return {
     title: String(seoSearchResults.items[0].fields.seoTitle),
-    description: String(seoSearchResults.items[0].fields.seoDescription),
+    description: String(seoSearchResults.items[0].fields.description),
     keywords: seoSearchResults.items[0].fields.seoKeywords as string[],
     openGraph: {
-      url: `https://www.grandbay-puntacana.com/trips/${slug}`,
+      url: "https://www.grandbay-puntacana.com/silverbank-expedition",
       type: "website",
       title: String(seoSearchResults.items[0].fields.seoTitle),
       description: String(seoSearchResults.items[0].fields.seoDescription),
@@ -63,60 +60,54 @@ export async function generateMetadata(
       ],
     },
     alternates: {
-      canonical: `https://www.grandbay-puntacana.com/trips/${slug}/`,
+      canonical: "https://www.grandbay-puntacana.com/silverbank-expedition/",
     },
   }
 }
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ slug: string }>
-}) {
-  const { slug } = await params
-  const tour = await searchEntries(
-    "tours",
-    {
-      "fields.slug": slug,
-    },
-    undefined,
-    [
-      "fishing-punta-cana",
-      "shark-dive-punta-cana",
-      "silverbank-expedition",
-      "whale-watching-adventure",
-    ],
-  )
-
+export default async function Home(props: any) {
+  const pageLayout = await searchEntries("tours", {
+    "fields.page": "Silverbank",
+  })
   return (
     <main>
       <BackgroundVideo
-        video={(tour.items[0] as any).fields.videoHero.fields.file.url}
+        video={(pageLayout.items[0] as any).fields.videoHero.fields.file.url}
         className={`-mt-20 md:-mt-40 [clip-path:polygon(0_0,100%_0,100%_35vh,0%_100%)] lg:[clip-path:polygon(0_0,100%_0,100%_55vh,0%_100%)]`}
       />
-      <div className="mb-5">
-        <div className="flex flex-col lg:flex-row lg:mx-auto max-w-6xl xl:h-[20rem] justify-center">
-          <div className="my-5 lg:flex lg:flex-col lg:justify-start lg:mt-5 xl:min-h-full xl:justify-center xl:mt-0">
-            <RichText context={tour.items[0].fields.paragraph1} />
+      <div className="my-5">
+        <div className="flex flex-col lg:flex-row lg:mx-auto max-w-7xl xl:space-x-10">
+          <div className="lg:flex lg:flex-col lg:justify-start lg:mt-5 xl:min-h-full xl:mt-0">
+            <RichText context={pageLayout.items[0].fields.paragraph1} />
+          </div>
+          {/* <div className="lg:w-[45rem] xl:mx-10 lg:min-h-full lg:flex lg:flex-col md:justify-start md:mt-2 lg:mt-7 2xl:mt-14">
+            <TripOverview tour={pageLayout.items[0].fields} />
+          </div> */}
+          <div className="lg:flex lg:flex-col lg:justify-start lg:mt-5 xl:min-h-full xl:mt-0">
+            <RichText context={pageLayout.items[0].fields.paragraph2} />
           </div>
         </div>
         <SwiperCarousel
-          photoList={(tour.items[0] as any).fields.photoList}
+          photoList={(pageLayout.items[0] as any).fields.photoList}
           className={`mt-5 [clip-path:polygon(0_5vh,100%_0,100%_30vh,0%_100%)] md:[clip-path:polygon(0_5vh,100%_0,100%_40vh,0%_100%)] lg:[clip-path:polygon(0_5vh,100%_0,100%_50vh,0%_100%)] xl:[clip-path:polygon(0_5vh,100%_0,100%_60vh,0%_100%)]`}
           height={`h-[35vh] md:h-[45vh] lg:h-[55vh] xl:h-[65vh]`}
         />
-        <div className="flex flex-col lg:flex-row lg:mx-auto max-w-6xl lg:h-[35rem]">
-          <div className="lg:flex lg:flex-col lg:justify-start xl:min-h-full xl:justify-center xl:mt-0">
-            <RichText context={tour.items[0].fields.paragraph2} />
-          </div>
-          <div className="lg:w-[45rem] xl:mx-10 lg:min-h-full lg:flex lg:flex-col lg:justify-center">
-            <TripOverview tour={tour.items[0].fields} />
-          </div>
-          <div className="lg:flex lg:flex-col lg:justify-start xl:min-h-full xl:justify-center xl:mt-0">
-            <RichText context={tour.items[0].fields.paragraph3} />
+        <div className="flex flex-col justify-center items-center xl:my-10">
+          <div className="flex flex-col max-w-7xl">
+            <div className="lg:flex items-start xl:space-x-10">
+              <RichText context={pageLayout.items[0].fields.paragraph3} />
+
+              <RichText context={pageLayout.items[0].fields.paragraph4} />
+            </div>
           </div>
         </div>
       </div>
+      <BackgroundVideo
+        video={
+          (pageLayout.items[0] as any).fields.secondaryVideo.fields.file.url
+        }
+        className={`[clip-path:polygon(0_5vh,100%_0,100%_40vh,0%_100%)] lg:[clip-path:polygon(0_5vh,100%_0,100%_60vh,0%_100%)]`}
+      />
     </main>
   )
 }
