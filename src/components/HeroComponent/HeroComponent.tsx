11b17bc5ffd05f5d54b1cfbcdcff28cede8d34ee
James Karnes <karnes.james@gmail.com>
2025-04-30 18:26:50 -0400

    testing hero image

--- src/components/HeroComponent/HeroComponent.tsx
+++ src/components/HeroComponent/HeroComponent.tsx
@@ -4,13 +4,13 @@ import { headers } from "next/headers"
 import { isMobile } from "@/utils/isMobile"
 import { getPlaiceholder } from "plaiceholder"
 const HeroComponent = async ({ heroImage }: { heroImage: string }) => {
-  // const headersList = await headers()
-  // const userAgent = headersList.get("user-agent")
-  // const mobileCheck = isMobile(userAgent)
-  // const buffer = await fetch(heroImage).then(async res => {
-  //   return Buffer.from(await res.arrayBuffer())
-  // })
-  // const { base64 } = await getPlaiceholder(buffer)
+  const headersList = await headers()
+  const userAgent = headersList.get("user-agent")
+  const mobileCheck = isMobile(userAgent)
+  const buffer = await fetch(heroImage).then(async res => {
+    return Buffer.from(await res.arrayBuffer())
+  })
+  const { base64 } = await getPlaiceholder(buffer)
   return (
     <div className="absolute top-0 w-full h-[55vh] lg:h-[80vh]">
       <div className="relative h-[55vh] lg:h-[80vh] [clip-path:polygon(0%_0%,100%_0%,100%_50vh,0%_100%)] lg:[clip-path:polygon(0%_0%,100%_0%,100%_75vh,0%_100%)]">
@@ -21,10 +21,9 @@ const HeroComponent = async ({ heroImage }: { heroImage: string }) => {
           height={1080}
           className="object-cover object-[40%_50%] z-0 w-full h-full"
           priority={true}
-          quality={65}
-          // quality={mobileCheck ? 65 : 80}
-          // placeholder="blur"
-          // blurDataURL={base64}
+          quality={mobileCheck ? 65 : 80}
+          placeholder="blur"
+          blurDataURL={base64}
           sizes="100vw"
           fetchPriority="high"
           loading="eager"
